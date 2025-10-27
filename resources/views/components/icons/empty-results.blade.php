ed1b31b71ed874b8ecc1cd32fe24c1e769b9c669
Andrej Prus <goleaf@gmail.com>
2025-10-27 17:30:43 +0200

    Ensure icon components default stroke width is defined

--- resources/views/components/icons/empty-results.blade.php
+++ resources/views/components/icons/empty-results.blade.php
@@ -1,5 +1,20 @@
-@props(['class' => 'h-6 w-6', 'stroke-width' => '1.5'])
+@php
+    /**
+     * Establishes default Tailwind sizing classes for the empty results icon.
+     *
+     * @var string $class
+     */
+    $class = $attributes->get('class', 'h-6 w-6');
 
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" {{ $attributes->merge(['class' => $class, 'stroke-width' => $strokeWidth]) }}>
+    /**
+     * Ensures the icon respects a configurable stroke width with a sensible default.
+     *
+     * @var string $strokeWidth
+     */
+    $strokeWidth = $attributes->get('stroke-width', '1.5');
+@endphp
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" {{ $attributes->merge(['class' =>
+$class, 'stroke-width' => $strokeWidth]) }}>
     <path stroke-linecap="round" stroke-linejoin="round" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
--- resources/views/components/icons/exclamation-circle.blade.php
+++ resources/views/components/icons/exclamation-circle.blade.php
@@ -1,5 +1,20 @@
-@props(['class' => 'h-6 w-6', 'stroke-width' => '1.5'])
+@php
+    /**
+     * Sets the default Tailwind sizing for the exclamation circle icon.
+     *
+     * @var string $class
+     */
+    $class = $attributes->get('class', 'h-6 w-6');
 
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" {{ $attributes->merge(['class' => $class, 'stroke-width' => $strokeWidth]) }}>
+    /**
+     * Keeps the alert icon stroke width overridable with a project-wide default.
+     *
+     * @var string $strokeWidth
+     */
+    $strokeWidth = $attributes->get('stroke-width', '1.5');
+@endphp
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" {{ $attributes->merge(['class' =>
+$class, 'stroke-width' => $strokeWidth]) }}>
     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
 </svg>
